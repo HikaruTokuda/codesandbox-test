cdb85f45f94777b2ba88b5fc6e500663d6eb2d9f
HikaruTokuda <33354706+HikaruTokuda@users.noreply.github.com>
2022-12-28 14:12:33 +0900

    template_strings

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 /**
- * const let等の変数宣言
+ * テンプレート文字列
  */
-// var val1 = "var変数";
-// console.log(val1);
-// // var変数は上書き可能
-// val1 = "var変数を上書き";
-// console.log(val1);
+const name = "なまえ";
+const age = 28;
+let message = "私の名前は" + name + "です。年齢は" + age + "です";
+// console.log(message);
 
-// // var変数は再宣言可能
-// var val1 = "var変数を再宣言";
-// console.log(val1);
-
-// let val2 = "let変数";
-// console.log(val2);
-// // letは上書きが可能
-// val2 = "let変数を上書き";
-// console.log(val2);
-// letは再宣言不可
-// let val2 = 'let変数を再宣言';
-
-// const val3 = "const変数";
-// console.log(val3);
-
-// const変数は上書き不可
-// val3 = 'const変数を上書き';
-// const val3 = 'const変数を再宣言';
-
-// vonstでもオブジェクトは編集可能
-// const val4 = {
-//   name: "なまえ",
-//   age: 28
-// };
-// // console.log(val4);
-// val4.name = "namae";
-// val4.address = "広島";
-// console.log(val4);
-
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+message = `私の名前は${name}です。年齢は${age}です`;
+console.log(message);
