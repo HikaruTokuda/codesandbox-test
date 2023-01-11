cb29dddfd93274fbd2df10a390577b99f21303b7
HikaruTokuda <33354706+HikaruTokuda@users.noreply.github.com>
2023-01-11 10:37:33 +0900

    default-arguments

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 /**
- * 分割代入
+ * デフォルト値、引数など
  */
+const sayHello = (name = 'ゲスト') => console.log(`こんにちは${name}さん`);
 
-const myProfile = {
-  name: "なまえ",
-  age: 28
-};
-
-let message1 = `1. 名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-console.log(message1);
-
-const { name, age } = myProfile;
-const message2 = `2. 名前は${name}です。年齢は${age}です`;
-console.log(message2);
-
-const myProfileArray = ["なまえ", 28];
-const message3 = `3. 名前は${myProfileArray[0]}です。年齢は${myProfileArray[1]}です`;
-console.log(message3);
-
-const [nameFromArray, ageFromArray] = myProfileArray;
-const message4 = `4. 名前は${nameFromArray}です。年齢は${ageFromArray}です。`;
-console.log(message4);
+sayHello()  // こんにちはゲストさん 
+sayHello('ひかる')  // こんにちはひかるさん 
