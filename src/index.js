1362d89a663e5985eb06bc865a93e31b0736d355
HikaruTokuda <33354706+HikaruTokuda@users.noreply.github.com>
2023-01-11 11:14:37 +0900

    logical operator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 /**
- * 三項演算子
+ * 論理演算子
  */
-const num = "1300";
-const formattedNum =
-  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
-// console.log(formattedNum);
+// const flag1 = true;
+// const flag2 = false;
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
-};
-console.log(checkSum(50, 30));
-console.log(checkSum(50, 60));
+// if (flag1 || flag2) console.log("1か2はtrueです");
+// if (flag1 && flag2) console.log("1も2もtrueです");
+
+// ||は左側がFALSEなら右側を返す
+// const num = 100;
+// const fee = num || '金額未設定です';
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
