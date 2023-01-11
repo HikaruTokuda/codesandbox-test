50d0d8e93afe6f92aba0a16df50e5c352e0c1a46
HikaruTokuda <33354706+HikaruTokuda@users.noreply.github.com>
2023-01-11 11:07:04 +0900

    ?

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 /**
- * mapやfilterを使った配列の処理
+ * 三項演算子
  */
+const num = "1300";
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
 
-const nameArr = ["田中", "山田", "トクダ"];
-//  for(let i = 0; i < nameArr.length; i++) {
-//    console.log(`${i}番目は${nameArr[i]}`);
-//  }
-
-//  const nameArr2 = nameArr.map((name) => {
-//    return name;
-//   });
-//   console.log(nameArr2);
-
-// nameArr.map( (name, index) => {
-//   console.log(`${index}番目は${name}`);
-// });
-
-// const numArr = [1,  2, 3, 4, 5];
-// const newNummArr = numArr.filter( (num) => {
-//   return num % 2 !== 0;
-// });
-// console.log(newNummArr);
-
-const newNameArr = nameArr.map((name) => {
-  if (name === "トクダ") return name;
-  else return `${name}さん`;
-});
-console.log(newNameArr);
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+};
+console.log(checkSum(50, 30));
+console.log(checkSum(50, 60));
